ad9601745b68b3d29363ec8d4eb6e9634ea42e2d
jakelucasnyc <jakelucasnyc@gmail.com>
2021-07-19 21:17:42 -0400

    working prototype with poll command

--- source/bot/__init__.py
+++ source/bot/__init__.py
@@ -17,20 +17,29 @@ async def on_ready():
     logger.info('Bot Ready')
 
 @bot.command()
-async def poll(ctx, *, arg):
+async def poll(ctx, *, arg=None):
+    logger.info('Poll Command Ran')
+    if arg is None:
+        await ctx.send('Command requires poll options. Input each parameter as a comma-separated list(e.g. /poll option one, option-two, third option !?@).')
+        return
+
     optionsList = arg.split(',') 
+    if len(optionsList) > 10:
+        await ctx.send('Too many options. Maximum of 10 options')
+        return
+
     message = ''
     for idx, option in enumerate(optionsList):
-        addedOption = f'{emojiDict[numList[idx]]}: {option}'
+        addedOption = f'{emojiDict[numList[idx]]} -> {option}'
         if idx < len(optionsList)-1:
-            addedOption += '\n'
+            addedOption += '\n\n'
         message += addedOption
 
     messageObj = await ctx.send(message)
     messageId = messageObj.id
     for reaction in range(len(optionsList)):
         addedEmoji = emojiDict[numList[reaction]]
-        await message.add_reaction(addedEmoji)
+        await messageObj.add_reaction(addedEmoji)
 
     
 
