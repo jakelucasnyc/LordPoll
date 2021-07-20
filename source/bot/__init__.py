02748cea166b48378ed7762722b675a481e35a0d
jakelucasnyc <jakelucasnyc@gmail.com>
2021-07-19 21:32:42 -0400

    adding file logging

--- source/bot/__init__.py
+++ source/bot/__init__.py
@@ -5,6 +5,9 @@ from emoji import emojize, demojize
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 intents = discord.Intents.default()
 
